d8d318da4f1e6808b3ebe093a34e14fed3fa7382
Dave O'Neill <daveoneill234@gmail.com>
2019-05-08 19:46:58 -0700

    Remove unused AppleAudioUnit.m

--- AppleAudioUnit/Classes/AppleAudioUnit.h
+++ AppleAudioUnit/Classes/AppleAudioUnit.h
@@ -5,17 +5,8 @@
 //  Created by David O'Neill on 2/10/19.
 //
 
-#import "AAUDsp.h"
 #import "BufferedAudioUnit.h"
 #import "ParameterizedAudioUnit.h"
 #import "AudioComponentDescription-Cwift.h"
 #import "EventSlicedAudioUnit.h"
 
-@interface AppleAudioUnit : ParameterizedAudioUnit
-@property (readonly) AAUDsp _Nullable dsp;
-- (void)setRenderer:(AAUDsp _Nonnull)renderer
-     renderCallback:(AAUDspRenderCallback _Nonnull)renderCallback
-       midiCallback:(AAUDspMidiCallback _Nullable)midiCallback
-   allocateCallback:(AAUDspAllocateCallback _Nullable)allocateCallback
-    releaseCallback:(AAUDspReleaseCallback _Nullable)releaseCallback;
-@end
